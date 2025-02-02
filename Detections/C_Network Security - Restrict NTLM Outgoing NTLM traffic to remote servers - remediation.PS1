57f5cfc9cd33c9ed8e2d751efc3be4baa00fe428
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2025-02-02 15:20:38 -0500

    Delete Detections/C_Network Security - Restrict NTLM Outgoing NTLM traffic to remote servers - remediation.PS1

--- Detections/C_Network Security - Restrict NTLM Outgoing NTLM traffic to remote servers - remediation.PS1
+++
@@ -1,2 +0,0 @@
-if((Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\MSV1_0") -ne $true) {  New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\MSV1_0" -force -ea SilentlyContinue };
-New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\MSV1_0' -Name 'RestrictSendingNTLMTraffic' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
