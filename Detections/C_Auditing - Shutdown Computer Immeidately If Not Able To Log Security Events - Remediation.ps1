9c35d292d53d89be96c88550901000e8f1b7bf35
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2025-02-02 15:20:32 -0500

    Delete Detections/C_Auditing - Shutdown Computer Immeidately If Not Able To Log Security Events - Remediation.ps1

--- Detections/C_Auditing - Shutdown Computer Immeidately If Not Able To Log Security Events - Remediation.ps1
+++
@@ -1,2 +0,0 @@
-if((Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa") -ne $true) {  New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -force -ea SilentlyContinue };
-New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa' -Name 'crashonauditfail' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
