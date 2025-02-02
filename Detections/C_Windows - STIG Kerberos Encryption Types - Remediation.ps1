89955e2c98ac56abf18b0931141cf1369fecb783
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2025-02-02 15:20:54 -0500

    Delete Detections/C_Windows - STIG Kerberos Encryption Types - Remediation.ps1

--- Detections/C_Windows - STIG Kerberos Encryption Types - Remediation.ps1
+++
@@ -1,3 +0,0 @@
-if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\kerberos\parameters") -ne $true) {  New-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\kerberos\parameters" -force -ea SilentlyContinue };
-New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\kerberos\parameters' -Name 'supportedencryptiontypes' -Value 0x2147483640 -PropertyType DWord -Force -ea SilentlyContinue;
-
