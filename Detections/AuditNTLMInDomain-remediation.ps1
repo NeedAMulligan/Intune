7d6d06c69481488e7ca6881b618fe9f863d46164
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2025-02-02 15:19:44 -0500

    Delete Detections/AuditNTLMInDomain-remediation.ps1

--- Detections/AuditNTLMInDomain-remediation.ps1
+++
@@ -1,2 +0,0 @@
-if((Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters") -ne $true) {  New-Item "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" -force -ea SilentlyContinue };
-New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters' -Name 'auditntlmindomain' -Value 7 -PropertyType DWord -Force -ea SilentlyContinue;
